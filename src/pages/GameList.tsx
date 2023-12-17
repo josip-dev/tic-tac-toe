3de57840de6dbd6abe425606f29956ca75ad8bb1
bracooo <bracooo@outlook.com>
2023-12-17 20:48:21 +0100

    Remove checkboxes implementation for statuses (API doesn't support status list parameter)

--- src/pages/GameList.tsx
+++ src/pages/GameList.tsx
@@ -10,7 +10,6 @@ import {
     GameStatus,
     getGameStatusString,
 } from '../constants/game-status';
-// import CheckBox from '../components/CheckBox';
 import { IconChartBar, IconEye, IconPlayerPlay } from '@tabler/icons-react';
 import Button from '../components/Button';
 import InputField from '../components/InputField';
@@ -23,29 +22,15 @@ const GameList = () => {
         useApiRequest<PaginatedResponse<Game>>('games');
 
     const [statusFilter, setStatusFilter] = useState<GameStatus | undefined>();
-    // const [filterOpenGames, setFilterOpenGames] = useState(true);
-    // const [filterInProgressGames, setFilterInProgressGames] = useState(true);
-    // const [filterFinishedGames, setFilterFinishedGames] = useState(true);
 
     useEffect(() => {
         fetchGames({
             limit: GAME_FETCH_LIMIT,
             offset: gameFetchOffset,
             status: statusFilter,
-            // status: [
-            //     filterOpenGames && GameStatus.Open,
-            //     filterInProgressGames && GameStatus.InProgress,
-            //     filterFinishedGames && GameStatus.Finished,
-            // ].filter(Boolean),
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [
-        gameFetchOffset,
-        statusFilter,
-        // filterOpenGames,
-        // filterInProgressGames,
-        // filterFinishedGames,
-    ]);
+    }, [gameFetchOffset, statusFilter]);
 
     return (
         <>
@@ -54,33 +39,6 @@ const GameList = () => {
             </h2>
 
             <div className="flex justify-between items-center mb-4">
-                {/* <fieldset className="w-fit border border-solid border-cyan-800 px-5 py-3">
-                    <legend className="text-sm px-2">Status</legend>
-
-                    <div className="flex gap-3 items-center">
-                        <CheckBox
-                            name="open"
-                            value={GameStatus.Open}
-                            label={getGameStatusString(GameStatus.Open)}
-                            isChecked={filterOpenGames}
-                            setIsChecked={setFilterOpenGames}
-                        />
-                        <CheckBox
-                            name="progress"
-                            value={GameStatus.InProgress}
-                            label={getGameStatusString(GameStatus.InProgress)}
-                            isChecked={filterInProgressGames}
-                            setIsChecked={setFilterInProgressGames}
-                        />
-                        <CheckBox
-                            name="finished"
-                            value={GameStatus.Finished}
-                            label={getGameStatusString(GameStatus.Finished)}
-                            isChecked={filterFinishedGames}
-                            setIsChecked={setFilterFinishedGames}
-                        />
-                    </div>
-                </fieldset> */}
                 <InputField
                     dropdownValues={GAME_STATUS_DROPDOWN_OPTIONS}
                     name="statusFilter"
